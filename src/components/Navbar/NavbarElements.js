2fd05f52f2922c838322b829480ad896c8380d75
Rafael Korz <rafael.korz@gmail.com>
2020-12-14 21:27:36 -0300

    Finished toggle Header

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -5,7 +5,7 @@ import { Link as LinkS } from 'react-router-dom'
 export const Nav = styled.nav`
   background: #000;
   height: 90px;
-  /* margin-top: -80px; */
+  margin-top: -80px;
   display: flex;
   justify-content: center;
   align-items: center;
